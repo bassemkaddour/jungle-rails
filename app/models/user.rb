1537b1d0039107c19db15604963c004e8e550f12
Bassem Kaddour <bassemkaddour@gmail.com>
2018-11-14 04:51:09 +0000

    tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   has_many :reviews
   validates :first_name, :last_name, :email, :password_digest, presence: true
   validates :email, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 8}
+  validates :password, length: { minimum: 1}
+
+  def self.authenticate_with_credentials(email, password) 
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user 
+    else 
+      nil
+    end 
+  end 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,15 +45,31 @@ RSpec.describe User, type: :model do
       #   ActiveRecord::RecordInvalid
       # )
     end
-
-    # it 'is not valid without quantity' do 
-    #   subject.quantity = nil
-    #   expect(subject).to_not be_valid 
-    # end
-
-    # it 'is not valid without category' do 
-    #   subject.category = nil
-    #   expect(subject).to_not be_valid 
-    # end
   end
+
+  describe '.authenticate_with_credentials' do 
+    subject {
+      described_class.new(
+        first_name: "Anything", 
+        last_name: 'Name',           
+        email: 'email@mail.bail', 
+        password: '12345678', 
+        password_confirmation: '12345678')
+    }
+    it 'authenticates a user with valid credentials' do 
+      subject.save! 
+      expect(described_class.authenticate_with_credentials(subject.email, subject.password)).to eql(subject)
+    end 
+
+    it 'authenticates a user with wrong case email with spaces' do 
+      subject.save! 
+      expect(described_class.authenticate_with_credentials(' EMAIL@MAIL.BaIl', subject.password)).to eql(subject)
+    end 
+
+    it 'returns nil for a user with the wrong credentials' do 
+      subject.save! 
+      expect(described_class.authenticate_with_credentials('MAIL@MAIL.BaIl', 'wrongpass')).to eql(nil)
+    end 
+
+  end 
 end
